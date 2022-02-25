d8c39042e0f727ea38c8e4dc00ce359e41851089
TranNhatQuang2405 <trannhatquang2405@gmail.com>
2022-02-25 11:07:43 +0700

    cai thu vien

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./configs/redux/Slice/DemoSlice";
 import "./App.css";
 function App() {
-    // @ts-ignore
-    const count = useSelector((state) => state.demo.value);
-    const dispatch = useDispatch();
-    return (
-        <div className="App">
-            <div>
-                <button
-                    aria-label="Increment value"
-                    onClick={() => dispatch(increment())}
-                >
-                    Increment
-                </button>
-                <span>{count}</span>
-                <button
-                    aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./configs/redux/store";
 import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
