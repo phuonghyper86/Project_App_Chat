56dab7c5447321b058682e40aaf2ea174b688373
phuonghyper86 <82032424+phuonghyper86@users.noreply.github.com>
2022-12-27 13:15:21 +0700

    change sound

--- src/layout/Provider/Context.js
+++ src/layout/Provider/Context.js
@@ -6,9 +6,8 @@ import { UpdateSerialId } from "configs/firebase/ServiceFirebase/ServiceInsert";
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
+//const socket = io('http://localhost:5000');
 const socket = io("https://tlcn.azurewebsites.net/");
-
 const ContextProvider = ({ children }) => {
     const user = useSelector((state) => state.UserInfo.user);
 
